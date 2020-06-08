bfc6910d9d3c20bfe2e82ac7adc7be12acf1230d
unknown <huyenpham@novahub.vn>
2020-06-08 21:59:51 +0700

    create class interface stack and write unit test for functions

---
+++ src/main/java/IStack.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class IStack<T> {
+
+    private List<T> stackNumber = new ArrayList<>();
+    
+    public void push(T x) {
+        stackNumber.add(x);
+    }
+
+    public T pop() {
+        
+        if (isEmpty()) {
+            return null;
+        }
+        int topIndex = stackNumber.size() - 1;
+
+        T result = stackNumber.get(topIndex);
+        stackNumber.remove(result);
+
+        return result;
+    }
+
+    public boolean isEmpty() {
+        return stackNumber.isEmpty();
+    }
+
+    public boolean clear() {
+        return stackNumber.removeAll(stackNumber);
+    }
+
+    public boolean contain(T x) {
+        return stackNumber.contains(x);
+    }
+
+    public T peek() {
+        int topIndex = stackNumber.size() - 1;
+        return isEmpty()? null : stackNumber.get(topIndex);
+    }
+}
---
+++ src/test/java/IStackTest.java
@@ -0,0 +1,88 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class IStackTest {
+
+    @Test
+    public void test_push() {
+       
+        IStack<Integer> stackNumber = new IStack<>();
+        IStack<String> stackString = new IStack<>();
+
+       stackNumber.push(10);
+       assertFalse(stackNumber.isEmpty());
+
+       stackString.push("test push string");
+       assertTrue(!stackString.isEmpty());
+    }
+
+    @Test
+    public void test_pop() {
+        
+        IStack<Integer> stackNumber = new IStack<>();
+
+        assertNull(stackNumber.pop());
+        
+        stackNumber.push(56);
+        stackNumber.push(65);
+        assertEquals(stackNumber.pop(), 65);
+    }
+
+    @Test
+    public void test_isEmpty() {
+
+        IStack<Integer> stackNumber = new IStack<>();
+
+        assertTrue(stackNumber.isEmpty());
+
+        stackNumber.push(5);
+        assertFalse(stackNumber.isEmpty());
+
+        stackNumber.pop();
+        assertTrue(stackNumber.isEmpty());
+    }
+
+    @Test
+    public void test_clear() {
+        
+        IStack<Integer> stackNumber = new IStack<>();
+
+        stackNumber.push(1);
+        stackNumber.push(65);
+        stackNumber.push(-6);
+        stackNumber.clear();
+
+        assertTrue(stackNumber.isEmpty());
+    }
+
+    @Test
+    public void test_contain() {
+
+        IStack<Integer> stackNumber = new IStack<>();
+        
+        stackNumber.push(1);
+        stackNumber.push(65);
+        stackNumber.push(-6);
+
+        assertTrue(stackNumber.contain(65));
+        assertFalse(stackNumber.contain(10));
+    }
+
+    @Test
+    public void test_peek() {
+        
+        IStack<Integer> stackNumber = new IStack<>();
+
+        assertNull(stackNumber.peek());
+
+        stackNumber.push(1);
+        stackNumber.push(65);
+        stackNumber.push(-6);
+
+        assertEquals(stackNumber.peek(), -6);
+    }
+}
